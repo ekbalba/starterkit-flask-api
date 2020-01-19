17f75957b3620ab6fdc08dac137a9f2c0be2787d
Errol Alba <ekbalba@outlook.com>
2020-01-19 14:16:56 -0800

    added dotenv_path

--- project_name/config.py
+++ project_name/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 # from os.path import dirname, join
 
-# dotenv_path = join(dirname(__file__), ".env")
-load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path=dotenv_path, verbose=True)
 
 
 def get_env_variable(variable_name):
