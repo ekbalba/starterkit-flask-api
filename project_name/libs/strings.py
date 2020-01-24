4696c9e41ae4fd41ddb6be74cdcee2fdb2d03b7f
Errol Alba <ekbalba@outlook.com>
2020-01-24 11:38:23 -0800

    fixed error

--- project_name/libs/strings.py
+++ project_name/libs/strings.py
@@ -3,7 +3,7 @@ libs.strings
 
 By default, uses `en-gb.json` file inside the `strings` top-level folder.
 
-If language changes, set `libs.strings.default_locale` and 
+If language changes, set `libs.strings.default_locale` and
 run `libs.strings.refresh()`.
 """
 import json
@@ -16,7 +16,7 @@ cached_strings = {}
 def refresh():
     print("Refreshing...")
     global cached_strings
-    if os.path.exists(open(f"project_name/strings/{default_locale}.json")):
+    if os.path.exists(f"project_name/strings/{default_locale}.json"):
         with open(f"project_name/strings/{default_locale}.json") as f:
             cached_strings = json.load(f)
     else:
