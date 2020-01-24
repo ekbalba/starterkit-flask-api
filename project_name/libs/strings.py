80a08eb158a92703fb5c1c295d2c106d854562df
Errol Alba <ekbalba@outlook.com>
2020-01-24 10:34:09 -0800

    fixed path not detected in project_name

--- project_name/libs/strings.py
+++ project_name/libs/strings.py
@@ -3,9 +3,11 @@ libs.strings
 
 By default, uses `en-gb.json` file inside the `strings` top-level folder.
 
-If language changes, set `libs.strings.default_locale` and run `libs.strings.refresh()`.
+If language changes, set `libs.strings.default_locale` and 
+run `libs.strings.refresh()`.
 """
 import json
+import os.path
 
 default_locale = "en-gb"
 cached_strings = {}
@@ -14,8 +16,12 @@ cached_strings = {}
 def refresh():
     print("Refreshing...")
     global cached_strings
-    with open(f"project_name/strings/{default_locale}.json") as f:
-        cached_strings = json.load(f)
+    if os.path.exists(open(f"project_name/strings/{default_locale}.json")):
+        with open(f"project_name/strings/{default_locale}.json") as f:
+            cached_strings = json.load(f)
+    else:
+        with open(f"strings/{default_locale}.json") as f:
+            cached_strings = json.load(f)
 
 
 def gettext(name):
