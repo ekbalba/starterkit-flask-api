e69efd6a87065a2d89d0c08c007adcb473545c08
Errol Alba <ekbalba@outlook.com>
2020-01-24 11:40:15 -0800

    removed validate_password

--- project_name/schemas/UserSchema.py
+++ project_name/schemas/UserSchema.py
@@ -3,9 +3,9 @@ from marshmallow import fields, validate
 from project_name.models.UserModel import UserModel
 
 validate_email = [validate.Email()]
-validate_password = [
-    validate.Regexp(regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).\S{8,}$")
-]
+# validate_password = [
+#     validate.Regexp(regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).\S{8,}$")
+# ]
 validate_name = [validate.Regexp(regex=r"^[a-zA-Z][a-zA-Z ]+$")]
 
 
@@ -13,7 +13,7 @@ class UserSchema(ma.ModelSchema):
     first_name = fields.Str(validate=validate_name, required=True)
     last_name = fields.Str(validate=validate_name, required=True)
     email = fields.Email(validate=validate_email, required=True)
-    password = fields.Str(validate=validate_password, required=True)
+    password = fields.Str(required=True)
 
     class Meta:
         model = UserModel
